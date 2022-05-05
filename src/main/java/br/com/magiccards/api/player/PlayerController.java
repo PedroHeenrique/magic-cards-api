7c75561b59224c19e3e39e325a6411861290ccfb
Pedro Henrique <pedroheenri_q@hotmail.com>
2022-05-05 18:15:01 -0300

    feat(PlayerController): Cria end point para registro novo jogador.

--- src/main/java/br/com/magiccards/api/player/PlayerController.java
+++ src/main/java/br/com/magiccards/api/player/PlayerController.java
@@ -1,14 +1,33 @@
 package br.com.magiccards.api.player;
+import br.com.magiccards.shared.domain.Player;
+import br.com.magiccards.shared.dto.NewPlayerForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/player")
 public class PlayerController {
+    private final PlayerService playerService;
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
 
     @PostMapping
-    public String hello(){
-        return "hello";
+    public ResponseEntity<Player> registerNewPlayer(@RequestBody @Valid NewPlayerForm  newPlayerForm) throws PlayerAlreadyExistException {
+        try{
+             Player playerSave = playerService.savePlayer(newPlayerForm);
+            return ResponseEntity.status(HttpStatus.CREATED).body(playerSave);
+        }catch (PlayerAlreadyExistException ex){
+            throw new PlayerAlreadyExistException();
+        }
+
     }
 }
