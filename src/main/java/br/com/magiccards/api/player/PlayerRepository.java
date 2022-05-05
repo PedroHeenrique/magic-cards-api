e63b4662dec80d3614da553aca04b8d9f82e3c64
Pedro Henrique <pedroheenri_q@hotmail.com>
2022-05-05 18:05:42 -0300

    feat(PlayerRepository): Cria metodo para buscar por username
    adiciona metodo ao repositorio para realizar busca por uername.

--- src/main/java/br/com/magiccards/api/player/PlayerRepository.java
+++ src/main/java/br/com/magiccards/api/player/PlayerRepository.java
@@ -4,6 +4,11 @@ import br.com.magiccards.shared.domain.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<Player,Long> {
+    Optional<Player> findByUsername(String username);
+
+
 }
