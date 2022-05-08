f0efca98c2ca330e338a710c74f0b5c0ebdfec84
Pedro Henrique <pedroheenri_q@hotmail.com>
2022-05-08 17:49:05 -0300

    feat(PlayerService/pom): Adiciona criptografia de senha
    Acrescenta dependencia para utilizacao de criptografia de senha, e
    implementa metodos para criptografar a senha do jogador.

--- src/main/java/br/com/magiccards/api/player/PlayerService.java
+++ src/main/java/br/com/magiccards/api/player/PlayerService.java
@@ -3,6 +3,7 @@ package br.com.magiccards.api.player;
 import br.com.magiccards.shared.domain.Player;
 import br.com.magiccards.shared.form.NewPlayerForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,18 +16,23 @@ public class PlayerService {
     public PlayerService(PlayerRepository playerRepository){
         this.playerRepository = playerRepository;
     }
-    public Player savePlayer(NewPlayerForm player) throws PlayerAlreadyExistException {
+
+    public Player savePlayer(Player player) throws PlayerAlreadyExistException {
         Optional<Player> optionalPlayer = playerRepository.findByUsername(player.getUsername());
 
         if(optionalPlayer.isPresent()){
             throw new PlayerAlreadyExistException();
         }
-        Player newPlayer = Player.builder()
-                                 .username(player.getUsername())
-                                 .password(player.getPassword())
-                                 .build();
-        return playerRepository.save(newPlayer);
+        String passwordEncoded = encodePassword(player.getPassword());
+        player.setPassword(passwordEncoded);
+        return playerRepository.save(player);
     }
+
+    private String encodePassword(String password){
+       return new BCryptPasswordEncoder().encode(password);
+    }
+
+
 
 
 }
