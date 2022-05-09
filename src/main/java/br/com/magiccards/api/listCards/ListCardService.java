8ff0958f7809fd2c564df992f23d6dd79668ce88
Pedro Henrique <pedroheenri_q@hotmail.com>
2022-05-08 21:31:45 -0300

    refactor(ListCadsService): promove melhoria na logica na classe no geral.

--- src/main/java/br/com/magiccards/api/listCards/ListCardService.java
+++ src/main/java/br/com/magiccards/api/listCards/ListCardService.java
@@ -1,9 +1,10 @@
 package br.com.magiccards.api.listCards;
 
-import br.com.magiccards.api.player.PlayerRepository;
+import br.com.magiccards.api.player.PlayerService;
 import br.com.magiccards.shared.domain.Card;
 import br.com.magiccards.shared.domain.ListCards;
 import br.com.magiccards.shared.domain.Player;
+import br.com.magiccards.shared.exception.player.PlayerInvalidException;
 import br.com.magiccards.shared.form.NewListCardsForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,45 +15,48 @@ import java.util.*;
 public class ListCardService {
 
     private final ListCardRepository listCardRepository;
-    private final PlayerRepository playerRepository;
+    private final PlayerService playerService;
 
     @Autowired
-    public ListCardService(ListCardRepository listCardRepository, PlayerRepository playerRepository) {
+    public ListCardService(ListCardRepository listCardRepository, PlayerService playerService) {
         this.listCardRepository = listCardRepository;
-        this.playerRepository = playerRepository;
+        this.playerService = playerService;
     }
 
-    public boolean saveListCardPlayer(NewListCardsForm newListCardsForm){
-        Optional<Player> player = playerRepository.findByUsername(newListCardsForm.getUsername());
+    public ListCards saveListCardPlayer(NewListCardsForm newListCardsForm) throws PlayerInvalidException {
+        Player playerParam = Player.builder().username(newListCardsForm.getUsername()).password(newListCardsForm.getPassword()).build();
+        ListCards listCards;
 
-        Player player1 = Player.builder().build();
-        if(player.isPresent()){
-            player1 = player.get();
-            ListCards listaCards = ListCards.builder()
-                    .listName(newListCardsForm.getListName())
-                    .player(player1)
-                    .build();
+        playerService.isValidPlayer(playerParam);
 
-            Set<ListCards> conjunto = new HashSet<>();
-            conjunto.add(listaCards);
-            player1.setListCards(conjunto);
+        Optional<Player> playerOpt = playerService.findPlayer(playerParam.getUsername());
+        Player playerFound = playerOpt.get();
 
-            List<Card> cartas = new ArrayList<>();
-            for ( Card carta: newListCardsForm.getLisCards()){
-                Card card = Card.builder().build();
-                card.setName(carta.getName());
-                card.setEdition(carta.getEdition());
-                card.setLanguage(carta.getLanguage());
-                card.setFoil(carta.getFoil());
-                card.setPrice(carta.getPrice());
-                card.setQuantityOfThisCard(carta.getQuantityOfThisCard());
-                card.setListCards(listaCards);
-                cartas.add(card);
-            }
+        listCards = ListCards.builder()
+                .listName(newListCardsForm.getListName())
+                .player(playerFound)
+                .build();
 
-            listaCards.setCards(cartas);
+        Set<ListCards> setOfCardsPlayer = new HashSet<>();
+        setOfCardsPlayer.add(listCards);
+        playerFound.setListCards(setOfCardsPlayer);
+
+        List<Card> cards = new ArrayList<>();
+        for ( Card carta: newListCardsForm.getLisCards()){
+            Card card = Card.builder().build();
+            card.setName(carta.getName());
+            card.setEdition(carta.getEdition());
+            card.setLanguage(carta.getLanguage());
+            card.setFoil(carta.getFoil());
+            card.setPrice(carta.getPrice());
+            card.setQuantityOfThisCard(carta.getQuantityOfThisCard());
+            card.setListCards(listCards);
+            cards.add(card);
         }
-        playerRepository.save(player1);
-        return true;
+        listCards.setCards(cards);
+        listCardRepository.save(listCards);
+
+
+    return listCards;
     }
 }
